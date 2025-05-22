ad20456bf3be7f81276b57b189434798c497f402
amir <a.nouri1997@gmail.com>
2025-05-22 17:21:58 +0330

    run router and create first pages

---
+++ src/pages/Index/Index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Index.css";
+
+export default function Index() {
+  return (
+    <div>
+      <h2>index</h2>
+    </div>
+  );
+}
---
+++ src/routes.jsx
@@ -0,0 +1,13 @@
+import ArticleInfo from "./pages/ArticleInfo/ArticleInfo";
+import Category from "./pages/Category/Category";
+import CourseInfo from "./pages/CourseInfo/CourseInfo";
+import Index from "./pages/Index/Index";
+
+const routes = [
+  { path: "/", element: <Index /> },
+  { path: "/article-info/:article-name", element: <ArticleInfo /> },
+  { path: "/course-info/:course-name", element: <CourseInfo /> },
+  { path: "/category-info/:category-name", element: <Category /> },
+];
+
+export default routes;
