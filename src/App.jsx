5f4acb28df0468d69cdfcd9654b1e128b53d8b75
amir <a.nouri1997@gmail.com>
2025-05-22 18:16:15 +0330

    create topbar and navbar and make styles folder with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import { useRoutes } from "react-router-dom";
 import routes from "./routes";
+import Header from "./components/Header/Header";
 function App() {
   const Router = useRoutes(routes);
-  return <>{Router}</>;
+  return (
+    <>
+      <Header />
+      {Router}
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import "./styles/variables.css";
+import "./styles/defaults.css";
+import "./styles/helpers.css";
 import "./index.css";
 import App from "./App.jsx";
 
