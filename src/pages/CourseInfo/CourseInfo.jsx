362d47fe8255b521808ffad8ca188319591e7753
amir <a.nouri1997@gmail.com>
2025-05-25 17:37:42 +0330

    change the teacher details palce to col-4

--- src/pages/CourseInfo/CourseInfo.jsx
+++ src/pages/CourseInfo/CourseInfo.jsx
@@ -308,38 +308,7 @@ export default function CourseInfo() {
               </div>
 
               {/* Finish Introduction  */}
-              {/* Start Teacher Details  */}
 
-              <div class="techer-details">
-                <div class="techer-details__header">
-                  <div class="techer-details__header-right">
-                    <img
-                      src="/images/info/teacher.jfif"
-                      alt="Teacher Profile"
-                      class="techer-details__header-img"
-                    />
-                    <div class="techer-details__header-titles">
-                      <a href="#" class="techer-details__header-link">
-                        محمدامین سعیدی راد
-                      </a>
-                      <span class="techer-details__header-skill">
-                        Front End & Back End Developer
-                      </span>
-                    </div>
-                  </div>
-                  <div class="techer-details__header-left">
-                    <i class="fas fa-chalkboard-teacher techer-details__header-icon"></i>
-                    <span class="techer-details__header-name">مدرس</span>
-                  </div>
-                </div>
-                <p class="techer-details__footer">
-                  اول از همه برنامه نویسی اندروید رو شروع کردم و نزدیک به 2 سال
-                  با زبان جاوا اندروید کار میکردم .بعد تصمیم گرفتم در زمینه وب
-                  فعالیت داشته باشم.و..
-                </p>
-              </div>
-
-              {/* Finish Teacher Details  */}
               {/* comments textArea */}
               <CommentsTextArea />
               {/* finish textArea */}
@@ -381,6 +350,39 @@ export default function CourseInfo() {
                     </div>
                   </div>
                 </div>
+                {/* Start Teacher Details  */}
+                <div class="course-info">
+                  <div class="techer-details">
+                    <div class="techer-details__header">
+                      <div class="techer-details__header-right">
+                        <img
+                          src="/images/info/teacher.jfif"
+                          alt="Teacher Profile"
+                          class="techer-details__header-img"
+                        />
+                        <div class="techer-details__header-titles">
+                          <a href="#" class="techer-details__header-link">
+                            محمدامین سعیدی راد
+                          </a>
+                          <span class="techer-details__header-skill">
+                            Front End & Back End Developer
+                          </span>
+                        </div>
+                      </div>
+                      <div class="techer-details__header-left">
+                        <i class="fas fa-chalkboard-teacher techer-details__header-icon"></i>
+                        <span class="techer-details__header-name">مدرس</span>
+                      </div>
+                    </div>
+                    <p class="techer-details__footer">
+                      اول از همه برنامه نویسی اندروید رو شروع کردم و نزدیک به 2
+                      سال با زبان جاوا اندروید کار میکردم .بعد تصمیم گرفتم در
+                      زمینه وب فعالیت داشته باشم.و..
+                    </p>
+                  </div>
+                </div>
+
+                {/* Finish Teacher Details  */}
                 <div class="course-info">
                   <div class="course-info__header-short-url">
                     <i class="fas fa-link course-info__short-url-icon"></i>
